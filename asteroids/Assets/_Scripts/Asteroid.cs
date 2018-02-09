4db8d56b206b187fc2a23bf192d0151c99c414e6
wytriam <thomas.m.frick@gmail.com>
2018-02-08 21:09:20 -0500

    Created player prefab, started creating asteroids

---
+++ asteroids/Assets/_Scripts/Asteroid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    float speed;
+    float rot;
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        speed = Random.Range(4, 6);
+        rot = Random.Range(0, 360);
+
+        rb = GetComponent<Rigidbody>();
+
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+        rb.AddForce(transform.up * speed, ForceMode.Acceleration);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rb.AddForce(transform.up * speed, ForceMode.Acceleration);
+    }
+}
