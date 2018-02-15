94a763b700a369f34218acd424bb64f42f445332
wytriam <thomas.m.frick@gmail.com>
2018-02-15 16:28:45 -0500

    you can now navigate the menu without using the mouse!

--- asteroids/Assets/_Scripts/menu_manager.cs
+++ asteroids/Assets/_Scripts/menu_manager.cs
@@ -12,6 +12,8 @@ public class menu_manager : MonoBehaviour
     public GameObject instructionsButton;
     public GameObject returnButton;
 
+    private GameObject selectButton;
+
 
 	// Use this for initialization
 	void Start ()
@@ -23,16 +25,44 @@ public class menu_manager : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+            SelectButton();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (selectButton != null)
+            {
+                selectButton.GetComponent<Button>().onClick.Invoke();
+            }
+        }
 	}
+
+    void SelectButton()
+    {
+        if (selectButton == null)
+        {
+            if (playButton.activeInHierarchy)
+                selectButton = playButton;
+            else
+                selectButton = returnButton;
+        }
+        else if (selectButton == playButton)
+            selectButton = instructionsButton;
+        else if (selectButton == instructionsButton)
+            selectButton = playButton;
+
+        selectButton.GetComponent<Button>().Select();
+
+    }
 
     public void PlayButton()
     {
+        selectButton = null;
         SceneManager.LoadScene(playScene);
     }
 
     public void InstructionsButton()
     {
+        selectButton = null;
         playButton.SetActive(false);
         instructionsButton.SetActive(false);
         returnButton.SetActive(true);
@@ -41,6 +71,7 @@ public class menu_manager : MonoBehaviour
 
     public void ReturnButton()
     {
+        selectButton = null;
         playButton.SetActive(true);
         instructionsButton.SetActive(true);
         returnButton.SetActive(false);
